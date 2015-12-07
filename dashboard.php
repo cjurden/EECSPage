ea704ce618db0e73ed668218776aca70c91af726
cjurden <colejurden@gmail.com>
2015-12-06 22:02:01 -0600

    dropdown

--- dashboard.php
+++ dashboard.php
@@ -186,19 +186,7 @@ function populateEvent(){
     <div class="container">
       <div class="row">
         <h3 class="col-md-offset-1">Select an Event: </h3>
-				<div class="dropdown">
-				  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-				    Dropdown
-				    <span class="caret"></span>
-				  </button>
-				  <!--<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-				    <li><a href="#">Action</a></li>
-				    <li><a href="#">Another action</a></li>
-				    <li><a href="#">Something else here</a></li>
-				    <li role="separator" class="divider"></li>
-				    <li><a href="#">Separated link</a></li>
-				  </ul>-->
-					<select class="dropdown-menu" aria-labelledby="dropdownMenu1">
+					<select class="form-control">
 	          <option disabled selected>
 	            Events
 	          </option>
@@ -206,7 +194,6 @@ function populateEvent(){
 	            populateEvent();
 	          ?>
 	        </select>
-				</div>
         <button class="btn btn-primary col-md-offset-3">Add Ticket</button>
       </div>
     </div>
