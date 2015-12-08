a06331d3fbfa86465dc7f10de0a62c6565c60152
cjurden <colejurden@gmail.com>
2015-12-07 22:44:39 -0600

    debugging jquery

--- dashboard.php
+++ dashboard.php
@@ -68,6 +68,7 @@
 			}
 		</style>
 		<script src="//d3js.org/d3.v3.min.js" charset="utf-8"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   </head>
   <body>
     <nav class="navbar navbar-default" role="navigation">
