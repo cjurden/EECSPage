1af85874cb0c4ed80ac35a0fe8cb23f1848d2898
cjurden <colejurden@gmail.com>
2015-12-07 19:27:16 -0600

    debugging writing tsv

--- dashboard.php
+++ dashboard.php
@@ -222,6 +222,7 @@ if($_POST['ticket'] == null || $_POST['file'] == null) {
 	}
 	$quer = "SELECT * FROM PRICE";
 	$array = mysql_query($quer);
+	var_dump($array);
 	write_tabbed_file('prices.tsv', $array, $save_keys, true);
 }
 
@@ -323,27 +324,26 @@ function populateAdmin() {
 		<div>
 			<div>
 				<svg id="visualization1" width="1000" heigh="500">
-					
+
 				</svg>
 			</div>
 		</div>
 		<div class="container">
 			<div class="row">
+				<script src="//d3js.org/d3.v3.min.js"></script>
 				<script>
 
-				var margin = {top: 20, right: 80, bottom: 30, left: 50},
+				var margin = {top: 20, right: 20, bottom: 30, left: 50},
 				    width = 960 - margin.left - margin.right,
 				    height = 500 - margin.top - margin.bottom;
 
-				var parseDate = d3.time.format("%Y%m%d").parse;
+				var parseDate = d3.time.format("%d-%b-%y").parse;
 
 				var x = d3.time.scale()
 				    .range([0, width]);
 
 				var y = d3.scale.linear()
 				    .range([height, 0]);
-
-				var color = d3.scale.category10();
 
 				var xAxis = d3.svg.axis()
 				    .scale(x)
@@ -354,9 +354,8 @@ function populateAdmin() {
 				    .orient("left");
 
 				var line = d3.svg.line()
-				    .interpolate("basis")
 				    .x(function(d) { return x(d.date); })
-				    .y(function(d) { return y(d.temperature); });
+				    .y(function(d) { return y(d.close); });
 
 				var svg = d3.select("body").append("svg")
 				    .attr("width", width + margin.left + margin.right)
@@ -364,30 +363,16 @@ function populateAdmin() {
 				  .append("g")
 				    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-				d3.tsv("PRICEDATA.csv", function(error, data) {
+				d3.tsv("prices.tsv", function(error, data) {
 				  if (error) throw error;
-
-				  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
 
 				  data.forEach(function(d) {
 				    d.date = parseDate(d.date);
-				  });
-
-				  var cities = color.domain().map(function(name) {
-				    return {
-				      name: name,
-				      values: data.map(function(d) {
-				        return {date: d.date, temperature: +d[name]};
-				      })
-				    };
+				    d.close = +d.close;
 				  });
 
 				  x.domain(d3.extent(data, function(d) { return d.date; }));
-
-				  y.domain([
-				    d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
-				    d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
-				  ]);
+				  y.domain(d3.extent(data, function(d) { return d.price; }));
 
 				  svg.append("g")
 				      .attr("class", "x axis")
@@ -402,25 +387,14 @@ function populateAdmin() {
 				      .attr("y", 6)
 				      .attr("dy", ".71em")
 				      .style("text-anchor", "end")
-				      .text("Temperature (ºF)");
+				      .text("Price ($)");
 
-				  var city = svg.selectAll(".city")
-				      .data(cities)
-				    .enter().append("g")
-				      .attr("class", "city");
-
-				  city.append("path")
+				  svg.append("path")
+				      .datum(data)
 				      .attr("class", "line")
-				      .attr("d", function(d) { return line(d.values); })
-				      .style("stroke", function(d) { return color(d.name); });
-
-				  city.append("text")
-				      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-				      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-				      .attr("x", 3)
-				      .attr("dy", ".35em")
-				      .text(function(d) { return d.name; });
+				      .attr("d", line);
 				});
+
 				</script>
 			</div>
 		</div>
